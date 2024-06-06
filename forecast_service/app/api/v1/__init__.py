ecdd3a29059ee16405d73c40a6648353d6b05a8c
mishaanikutin <billyknife@mail.ru>
2024-06-07 01:03:46 +0300

    Подправил Swagger

--- forecast_service/app/api/v1/__init__.py
+++ forecast_service/app/api/v1/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from .handlers import forecast_router
 
 
-router = APIRouter()
-router.include_router(forecast_router)
+router_v1 = APIRouter()
+router_v1.include_router(forecast_router)
 
-__all__ = ['router']
+__all__ = ['router_v1']
--- forecast_service/app/main.py
+++ forecast_service/app/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.api.v1 import router
+from app.api.v1 import router_v1
 
 
 """Запуск API Для прогнозного сервиса"""
 
 
-forecast_service = FastAPI(title="iep-forecast-service")
+forecast_service = FastAPI(
+    title="iep-forecast-service",
+    description="Сервис для прогноза макроиндексов",
+    version="1.0",
+    docs_url='/docs',
+    openapi_url='/openapi.json',
+    redoc_url=None
+)
 
 forecast_service.add_middleware(
     CORSMiddleware,
@@ -17,4 +24,4 @@ forecast_service.add_middleware(
     allow_headers=["*"],
 )
 
-forecast_service.include_router(router)
+forecast_service.include_router(router_v1, prefix="/v1")
