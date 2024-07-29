ca89a764a69d337b466ed0d0cbe64a7cacc46bd9
mishaanikutin <billyknife@mail.ru>
2024-07-29 15:26:42 +0300

    Добавил кэш в ендпоинты

--- forecast_service/app/api/v1/handlers/make_forecast.py
+++ forecast_service/app/api/v1/handlers/make_forecast.py
@@ -1,46 +1,26 @@
 from fastapi import APIRouter, HTTPException
+from fastapi_cache.decorator import cache
 
 from app.service.forecast_models import IPCForecast, IPPForecast, BaseForecastService
-
-from app.schemas import (
-    IndexFeaturesMapper,
-    ForecastResponse,
-    FeaturesResponse,
-    IPPRequestCB,
-    BaseRequest,
-    ReadyOnModels
-)
-from app.schemas.io.request import IPCRequestCB
-from app.schemas import IPPFeaturesResponse, IPCFeaturesResponse
+from app.schemas import (IndexFeaturesMapper, ForecastResponse, FeaturesResponse,
+                         IPPRequestCB, BaseRequest, ReadyOnModels, IPCRequestCB)
 
 
 forecast_router = APIRouter()
 
 
-# @forecast_router.get("/{index}/features_list")
-# async def features_list(index: ReadyOnModels):
-#     """# Получить список названий всех признаков необходимых для обучения модели"""
-#
-#     try:
-#         return IndexFeaturesMapper[index]()
-#     except KeyError:
-#         raise HTTPException(status_code=404, detail='Такого индекса нет')
-
-@forecast_router.get("/ipp/features_list")
-async def features_list():
+@forecast_router.get("/{index}/features_list")
+async def features_list(index: ReadyOnModels) -> FeaturesResponse:
     """# Получить список названий всех признаков необходимых для обучения модели"""
 
-    return IPPFeaturesResponse()
+    try:
+        return IndexFeaturesMapper[index]
+    except KeyError:
+        raise HTTPException(status_code=404, detail='Такого индекса нет')
 
 
-@forecast_router.get("/ipc/features_list")
-async def features_list():
-    """# Получить список названий всех признаков необходимых для обучения модели"""
-
-    return IPCFeaturesResponse()
-
-
-@forecast_router.post("/base")
+@forecast_router.get("/base")
+@cache(expire=3600)
 async def base_forecast(request: BaseRequest) -> ForecastResponse:
     """# Базовая модель для прогноза"""
 
@@ -51,7 +31,8 @@ async def base_forecast(request: BaseRequest) -> ForecastResponse:
             .predict())
 
 
-@forecast_router.post("/ipp/catboost")
+@forecast_router.get("/ipp/catboost/")
+@cache(namespace="make_forecast", expire=3600)
 async def cb_ipp_forecast(request: IPPRequestCB) -> ForecastResponse:
     """
     # Прогнозирование индекса промышленного производства с CatBoost
@@ -86,7 +67,8 @@ async def cb_ipp_forecast(request: IPPRequestCB) -> ForecastResponse:
             .predict())
 
 
-@forecast_router.post("/ipc/catboost")
+@forecast_router.get("/ipc/catboost")
+@cache(namespace="make_forecast", expire=3600)
 async def cb_ipc_forecast(request: IPCRequestCB) -> ForecastResponse:
     """
     # Прогнозирование индекса потребительских цен с CatBoost
